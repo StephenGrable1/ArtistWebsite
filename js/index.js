d3c0770743851a99d9db5cfb986af869cfa3486e
StephenGrable1 <stephengrable@gmail.com>
2016-11-21 23:08:29 -0500

    remove media fade in

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
         this.casheDom();
         this.scrollOnLoad();
         this.scrollOnClick();
-        this.fadeIn();
       },
       casheDom: function() {
         this.$el = $('html, body');
@@ -16,19 +15,6 @@ $(document).ready(function () {
               var href = $(this).parent().attr('href');
               handleScroll.scrollToSection(href);
           })
-      },
-      fadeIn: function() {
-        $(window).scroll(function() {
-            $('.media').each(function(i) {
-              var bottomSection = $(this).position().top + $(this).outerHeight();
-              var bottomWindow = $(window).scrollTop() + $(window).height();
-
-              if(bottomWindow > (bottomSection - 300)) {
-                //console.log("does this call only 5 times");
-                $(this).animate({'opacity':'1'}, 50);
-              }
-            });
-          });
       },
       scrollToSection: function(href) {
         this.$el.animate({
