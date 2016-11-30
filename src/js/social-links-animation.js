9a671e497e44d09861df87fefa12a80ba48f00c1
StephenGrable1 <stephengrable@gmail.com>
2016-11-29 20:50:20 -0500

    Replace jquery animation with velocity.js

--- src/js/social-links-animation.js
+++ src/js/social-links-animation.js
@@ -6,55 +6,29 @@ $(document).ready(function(){
     },
     casheDom: function(){
       this.$el = $('.follow-container');
-      this.$img = this.$el.find('img');
+      this.$sectionPosition = this.$el.position().top;
       this.$imgContainer = this.$el.find('.img-container');
-      this.$imgPosition = this.$el.position().top + this.$el.outerHeight();
     },
     bindEvents: function() {
-      this.watchWindow(this.$imgPosition, this.$img);
+      var socialButtons = this.$imgContainer;
+      var sectionPosition = this.$sectionPosition;
+      this.watchWindow(sectionPosition, socialButtons);
     },
-    watchWindow: function(position, images) {
-      var i = 0;
-        $(window).scroll(function() {
+    watchWindow: function(elementPosition, elementContainer) {
+        $(window).on('scroll',function() {
             var bottomWindow = $(window).scrollTop() + $(window).height();
-            if(bottomWindow > (position - 40) && (i===0)) {
-                console.log("this should only be called once: ", i);
-                socialLogoAnimation.queueIntroAnimataion(images);
-                i++;
-                return i;
-              }
+             console.log(bottomWindow, elementPosition);
+
+             if (bottomWindow > elementPosition) {
+                 console.log("this should only be called once");
+                 socialLogoAnimation.animateSocialButtons(elementContainer);
+                 $(window).off('scroll');
+               }
           });
     },
-    queueIntroAnimataion: function(images) {
-      socialLogoAnimation.fadeIn();
-      socialLogoAnimation.swingUpTimer(images);
-    },
-    swingUpTimer: function(images) {
-      var delay = 0;
-      $(images).each(function(){
-        //console.log("this should only fire 5 times and no more");
-          var id = this.id;
-          setTimeout(function() {
-            socialLogoAnimation.swingUp(id);
-          }, delay);
-          delay += 50;
-      });
-    },
-    fadeIn: function() {
-      $(this.$el.find('.social-links-container')).css('opacity','1');
-      console.log('fade in')
-    },
-    swingUp: function(id) {
-      $("#" + id).css({'margin-top':'15px'}, {duration:1000});
-
-      setTimeout(function() {
-        socialLogoAnimation.swingDown(id);
-        console.log('swingup: ', id)
-      }, 500);
-    },
-    swingDown: function(id) {
-      console.log("swing down", id);
-      $("#" + id).css({'margin-top':'+30px'}, {duration:800});
+    animateSocialButtons: function(imageContainer){
+      console.log(imageContainer);
+      $("img").velocity("transition.slideUpIn", {stagger: 75, drag: true });
     }
   }
   socialLogoAnimation.init();
