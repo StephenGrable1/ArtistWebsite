2410cab73bd7855f9b0ca9eee865039b62d8f48b
StephenGrable1 <stephengrable@gmail.com>
2016-11-29 22:22:24 -0500

    Cache calls to .followBtn in social button animation

--- src/js/social-links-animation.js
+++ src/js/social-links-animation.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
     casheDom: function(){
       this.$el = $('.follow-container');
       this.$sectionPosition = this.$el.position().top;
-      this.$imgContainer = this.$el.find('.img-container');
+      this.$followBtn = this.$el.find('.followBtn');
     },
     bindEvents: function() {
-      var socialButtons = this.$imgContainer;
+      var socialButtons = this.$followBtn;
       var sectionPosition = this.$sectionPosition;
       this.watchWindow(sectionPosition, socialButtons);
     },
@@ -19,7 +19,7 @@ $(document).ready(function(){
             var bottomWindow = $(window).scrollTop() + $(window).height();
 
              if (bottomWindow > elementPosition) {
-                 socialLogoAnimation.animateButtons(".followBtn");
+                 socialLogoAnimation.animateButtons(elementContainer);
                  $(window).off('scroll');
                }
           });
